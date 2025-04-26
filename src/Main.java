3370b418fd3a94af1c809da20ab48b5696387674
yamal <yamal_07@mail.ru>
2025-04-26 11:36:34 +0900

    Zadanie-5 , пропустил 3 - е задание его написал в конце

--- src/Main.java
+++ src/Main.java
@@ -13,9 +13,9 @@ public class Main {
         int clientOS = 0;
         int clientDeviceYear = 2015;
 
-        if (clientOS == 0 && clientDeviceYear <= 2015) {
+        if (clientOS == 0 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (clientOS == 1 && clientDeviceYear <= 2015) {
+        } else if (clientOS == 1 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
@@ -35,53 +35,59 @@ public class Main {
             deliveryDays += 2;
             System.out.println("Потребуется дней: " + deliveryDays);
         } else if (deliveryDistance > 20) {
-            deliveryDays += 1;
+            deliveryDays += 2;
             System.out.println("Потребуется дней: " + deliveryDays);
         } else {
             System.out.println("Потребуется дней: " + deliveryDays);
         }
 
 
-        int mothNumber = 12;
-        switch (mothNumber) {
-            case 1:
-            case 2:
-            case 12:
-                System.out.println("Зима");
-                break;
-            case 3:
-            case 4:
-            case 5:
-                System.out.println("Весна");
-                break;
-            case 6:
-            case 7:
-            case 8:
-                System.out.println("Лето");
-                break;
-            case 9:
-            case 10:
-            case 11:
-                System.out.println("Осень");
-                break;
-            default:
-                System.out.println("Такого месяца не существует");
+        int monthNumber = 12;
+
+        if (monthNumber < 1 || monthNumber > 12) {
+            System.out.println("Некорректный номер месяца (должен быть от 1 до 12)");
+        } else {
+            switch (monthNumber) {
+                case 1:
+                case 2:
+                case 12:
+                    System.out.println("Зима");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    System.out.println("Весна");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    System.out.println("Лето");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    System.out.println("Осень");
+                    break;
+            }
+
+            int year = 2024;
+            if (year % 400 == 0) {
+                System.out.println(year + " год является високосным");
+            } else if (year % 100 == 0) {
+                System.out.println(year + " год не является високосным");
+            } else if (year % 4 == 0) {
+                System.out.println(year + " год является високосным");
+            } else {
+                System.out.println(year + " год не является високосным");
+            }
         }
-
-
-                int year = 2024;
-                if (year % 400 == 0) {
-                    System.out.println(year + " год является високосным");
-                } else if (year % 100 == 0) {
-                    System.out.println(year + " год не является високосным");
-                } else if (year % 4 == 0) {
-                    System.out.println(year + " год является високосным");
-                } else {
-                    System.out.println(year + " год не является високосным");
-                }
-        }
-
     }
+}
+
+
+
+
+
 
 
 
